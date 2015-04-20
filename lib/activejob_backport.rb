6eba33bf86c2e57f61669f292aeb40c852dbb7a2
Paweł J. Wal <p@paweljw.al>
2015-04-20 16:24:22 +0200

    Remove a debug output

--- lib/activejob_backport.rb
+++ lib/activejob_backport.rb
@@ -1,8 +1,6 @@
 # if ActiveJob is not defined, we probably need the newer ActionMailer too
 if !defined?(ActiveJob)
   require 'active_job'
+  require_relative './action_mailer'
 end
-
-puts "requiring " + File.join( File.expand_path(File.dirname(__FILE__)), 'action_mailer')
-require File.join( File.expand_path(File.dirname(__FILE__)), 'action_mailer')
 
